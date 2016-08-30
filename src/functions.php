783ed9b95d25d39a532f1beeed4d0f900c44b4ec
Dylan Simowitz <dsimowitz@gmail.com>
2016-08-29 23:45:38 -0700

    enqueue bundle.js

--- src/functions.php
+++ src/functions.php
@@ -1,3 +1,11 @@
 <?php
-add_theme_support( 'post-thumbnails' );
-?>
+
+add_theme_support('post-thumbnails');
+
+function reaxtory_scripts()
+{
+    wp_register_script('bundle', get_template_directory_uri().'/build/bundle.js', array(), '', true);
+    wp_enqueue_script('bundle');
+}
+
+add_action('wp_enqueue_scripts', 'reaxtory_scripts');
